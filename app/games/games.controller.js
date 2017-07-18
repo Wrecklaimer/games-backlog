1077d10699df5569e4746bac0f82c2e21f8f1c4b
Kevin Diefenbach <wrecklaimer@gmail.com>
2017-07-18 15:24:12 -0400

    Add table sorting

--- app/games/games.controller.js
+++ app/games/games.controller.js
@@ -1,5 +1,9 @@
 angular.module('app.controllers').controller('GamesCtrl', ['$scope', 'apiService',
 	function ($scope, apiService) {
+	$scope.sort = {
+		type: 'release_date',
+		reverse: false
+	};
 	$scope.search = {};
 
 	apiService.getGames().then(function(games) {
@@ -8,5 +12,16 @@ angular.module('app.controllers').controller('GamesCtrl', ['$scope', 'apiService
 
 	$scope.clearSearch = function() {
 		$scope.search = {};
+	};
+
+	$scope.sortBy = function(type) {
+		// Sorting by the same type again will simply reverse the order
+		if ($scope.sort.type === type) {
+			$scope.sort.reverse = !$scope.sort.reverse;
+			return;
+		}
+
+		$scope.sort.type = type;
+		$scope.sort.reverse = false;
 	};
 }]);
