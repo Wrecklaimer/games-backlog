c25eaf3bda98239e1026e9d47e7b8f4587793720
Kevin Diefenbach <wrecklaimer@gmail.com>
2017-09-08 14:18:47 -0400

    Add filter to hide finished games

--- app/games/games.controller.js
+++ app/games/games.controller.js
@@ -4,11 +4,25 @@ angular.module('app.controllers').controller('GamesCtrl', ['$scope', 'apiService
 		type: 'release_date',
 		reverse: false
 	};
+	$scope.hideFinished = false;
 	$scope.search = {};
 
 	apiService.getGames().then(function(games) {
 		$scope.games = games.data;
 	});
+
+	$scope.filterGames = function(item) {
+		var matchTitle = true;
+		var matchStatus = true;
+
+		if ($scope.search.title)
+			matchTitle = item.title.toLowerCase().includes($scope.search.title.toLowerCase());
+		
+		if ($scope.hideFinished)
+			matchStatus = item.game_status !== 'Finished';
+
+		return matchTitle && matchStatus;
+	};
 
 	$scope.clearSearch = function() {
 		$scope.search = {};
